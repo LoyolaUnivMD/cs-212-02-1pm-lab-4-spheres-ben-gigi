34eca17a0f26e1a8d0e74b58edbf85572b455506
Gigi <gli@loyola.edu>
2024-02-15 15:35:10 -0500

    finalized code; added algorithm, comments, and design

--- Sphere.java
+++ Sphere.java
@@ -1,13 +1,11 @@
-
-/** Programmers:
+/** Programmers: Ben Zakielarz and Gigi Li
 * Course:  CS 212
-* Due Date:
-* Lab Assignment:
-* Problem Statement:
-* Data In:
-* Data Out:
-* Credits: [Is your code based on an example in the book, in class, or something else?
-*            Reminder: you should never take code from the Internet or another person
+* Due Date: 02/18/2024
+* Lab Assignment: 4
+* Problem Statement: Create a new class named Sphere and use it to create sphere objects and to detect sphere collisions
+* Data In: none
+* Data Out: Sphere's surface area, volume, distance from its center, and whether the two spheres collide
+* Credits: none
 */
 
 import java.lang.Math;
