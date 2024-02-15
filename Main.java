1428b6a0375cbb443538ef0e2d1b690dbc0eaf0f
Gigi <gli@loyola.edu>
2024-02-15 15:37:57 -0500

    commit

--- Main.java
+++ Main.java
@@ -10,8 +10,38 @@
 
 class Main {
     public static void main(String[] args) {
-       System.out.println("Music of the Spheres");
+        //tell the user the purpose of the program
+        System.out.println("This program will show you the distance from the center, surface area, and volume of the Sun, Earth, and PlanetX, including whether the objects will collide or not.\n");
 
+        //spherical objects
+        Sphere sun = new Sphere(43.26, 0, 0, 0);
+        Sphere earth = new Sphere(.395, 26, 42, 79);
+        Sphere planetX = new Sphere(1.4, 25.3, 43.1, 77.8);
 
+        //output the distance from the center of each spherical object
+        System.out.println("Distances from center, sun, earth, planetX");
+        System.out.println("Sun: " + Sphere.calcDistanceFromCenter(sun));
+        System.out.println("Earth: " + Sphere.calcDistanceFromCenter(earth));
+        System.out.println("PlanetX: " + Sphere.calcDistanceFromCenter(planetX));
+
+        //output the surface area of each spherical object
+        System.out.println("\nCalculates the surface area of the sun, earth, and planetX");
+        System.out.println("Sun: " + Sphere.calcSurfaceArea(sun));
+        System.out.println("Earth: " + Sphere.calcSurfaceArea(earth));
+        System.out.println("PlanetX: " + Sphere.calcSurfaceArea(planetX));
+
+        //output the volume of each spherical object
+        System.out.println("\nVolume of the Sun, Earth, and PlanetX");
+        System.out.println("Sun: " + Sphere.calcVolume(sun));
+        System.out.println("Earth: " + Sphere.calcVolume(earth));
+        System.out.println("PlanetX: " + Sphere.calcVolume(planetX));
+
+        //output whether the two spherical objects are colliding
+        System.out.println("\nCheck whether the spherical objects are colliding: ");
+        System.out.println("Sun and Earth: " + Sphere.isColliding(sun, earth));
+        System.out.println("Earth and PlanetX: " + Sphere.isColliding(earth, planetX));
+        System.out.println("PlanetX and Sun: " + Sphere.isColliding(planetX, sun));
+
+        System.out.println("\n Thank you for using this program.");
     }
 }
--- Sphere.java
+++ Sphere.java
@@ -10,9 +10,51 @@
 
 import java.lang.Math;
 class Sphere {
+    //data fields
+    double r;
+    double x;
+    double y;
+    double z;
+
+    //default values
+    public Sphere(double r, double x, double y, double z){
+        //constructor
+        this.r = r;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //method to calculate the surface area of the sphere
+    static double calcSurfaceArea(Sphere sphere) {
+        double area = 4 * Math.PI * Math.pow(sphere.r, 2);
+        return area;
+    }
+
+    //method to calculate the volume of the sphere
+    static double calcVolume(Sphere sphere) {
+        double volume = (Math.pow(sphere.r, 3)) * ((double) 4 /3) * Math.PI;
+        return volume;
+    }
+
+    //method to calculate the distance from the center of the sphere to the arbitrary point
+    static double calcDistanceFromCenter (Sphere sphere) {
+        //let Q equal an arbitrary point in space
+        int[] q = { 0, 0, 0 };
+        double distance = Math.sqrt(Math.pow(sphere.x-q[0], 2) + Math.pow(sphere.y-q[1], 2) + Math.pow(sphere.z-q[2], 2));
+        return distance;
+    }
+
+    //method to determine whether the two spheres will collide
+    static boolean isColliding(Sphere sphere1, Sphere sphere2) {
+        if(Math.abs(sphere1.calcDistanceFromCenter(sphere1) - sphere2.calcDistanceFromCenter(sphere2)) < (sphere1.r + sphere2.r)){
+            return true;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
        System.out.println("Spheres Go Here");
-
-
     }
 }
